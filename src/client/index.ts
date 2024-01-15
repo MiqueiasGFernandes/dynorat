574827805a5d4bec936b646f38ebbfd504b49569
miqueias.fernandes@bitfy.app <miqueiasgomesfernandes@gmail.com>
2024-01-15 19:13:45 -0300

    style: remove old code

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,56 +1,9 @@
-// import { config } from 'dotenv'
-// import { Server } from 'socket.io'
-
 import { DataSource } from 'typeorm'
 import { PresentersFactory } from './boostrap/Presenters.factory'
 import { DatabaseConfig } from './db/Config'
 import { TemplateView } from './views/Template.view'
 import packageData from '../../package.json'
 import { EventListener } from './event/EventListener'
-
-// const { default: inquirer } = require('fix-esm').require('inquirer')
-
-// async function connect (): Promise<void> {
-//   config()
-
-//   const client = new Server()
-
-//   console.log(process.env.PORT)
-
-//   const handler = client.listen(Number(process.env.PORT))
-
-//   console.log('Listening...')
-
-//   handler.on('connection', (socket) => {
-//     console.log('Connected')
-
-//     socket.emit('command', 'echo -n $PS1')
-
-//     socket.on('error', (error) => {
-//       console.log(error)
-//     })
-//     socket.on('result', async (data: unknown) => {
-//       const { command } = await inquirer.prompt([
-//         {
-//           name: 'command',
-//     })
-//     socket.on('result', async (data: unknown) => {
-//       const { command } = await inquirer.prompt([
-//         {
-//           name: 'command',
-//           prefix: '',
-//           sufix: '',
-//           message: data
-//         }
-//       ])
-
-//       client.emit('command', `${command} && echo -n $PS1`)
-//     })
-//   })
-// }
-
-// connect()
-
 const dataSource = new DataSource(DatabaseConfig.configure())
 
 void dataSource.initialize().catch((error) => {
