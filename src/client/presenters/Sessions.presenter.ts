16618da8cd4412c2101b345f97829aef0ca851b8
miqueias.fernandes@bitfy.app <miqueiasgomesfernandes@gmail.com>
2024-01-15 19:16:22 -0300

    build: install cli-table

--- src/client/presenters/Sessions.presenter.ts
+++ src/client/presenters/Sessions.presenter.ts
@@ -1,12 +1,14 @@
 import { Server } from 'socket.io'
-
+import Table from 'cli-table'
 export class SessionsPresenter {
   showSessions (): void {
     const client = new Server()
 
     const handler = client.listen(4444)
 
-    console.log('Listening...')
+    const table = new Table({
+      head: ['# ID', 'IP Address', 'Port', 'Country', 'OS', 'User']
+    })
 
     handler.on('connection', (socket) => {
       const address = socket.handshake
