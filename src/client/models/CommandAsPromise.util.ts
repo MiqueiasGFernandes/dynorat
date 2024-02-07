6a90b558a06db0f74ae16dc6f7314abf01eb75e1
miqueias.fernandes@bitfy.app <miqueiasgomesfernandes@gmail.com>
2024-02-07 16:07:04 -0300

    feat: add command error logging

--- src/client/models/CommandAsPromise.util.ts
+++ src/client/models/CommandAsPromise.util.ts
@@ -2,6 +2,11 @@ import { execSync } from 'node:child_process'
 
 export class Commander {
   static command (command: string): string {
-    return execSync(command).toString()
+    try {
+      return execSync(command).toString()
+    } catch (error: any) {
+      console.error(error.stderr.toString())
+      throw new Error(error.message as string)
+    }
   }
 }
